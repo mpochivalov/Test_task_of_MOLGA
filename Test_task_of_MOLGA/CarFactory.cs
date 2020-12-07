85ab74252a31acdb1a0bc52a7a03d4b46bbba711
Mvpochivalov <pochivalov.mikhail@gmail.com>
2020-12-07 10:09:15 +0300

    Comment code "CarFactory"

--- Test_task_of_MOLGA/CarFactory.cs
+++ Test_task_of_MOLGA/CarFactory.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Test_task_of_MOLGA
 {
+    //Реализация паттерна Singleton.
+    //В городе можно иметь только один завод по производству машин
+    //Class  CarFactory является реализацией паттерна, проверяется наличие завода в городе
+    //и осуществляется проверка на единственность экземпляра
     class City
     {
         public string NameOfCity { get; }
